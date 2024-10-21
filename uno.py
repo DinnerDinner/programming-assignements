f72e2ce341c75ae417753fc10e48fe66e05fc974
Pranjwal Singh <pranjwal.singh@dawsoncollege.qc.ca>
2024-10-21 17:15:35 -0400

    team merge

--- uno.py
+++ uno.py
@@ -1,59 +1,125 @@
+#Fabio and Sebastian and Pranjwal ;;;; we merged +2 and UNO Call
+
 import random
 
 def start_game():
-    colours = ("Red", "Yellow", "Green", "Blue")
-    ranks = list(range(1, 11)) + ["+2"]  # Add "+2" as a special rank
-    deck = [(rank, colour) for rank in ranks for colour in colours]
+    # the setup of uno
+    colours = ("red", "yellow", "green", "blue")
+    ranks = list(range(1,11)) + ["+2"]  # Add "+2" as a special rank
+
+    deck = [(colour, rank) for colour in colours for rank in ranks]
+
+    # shuffle the deck
+
     random.shuffle(deck)
 
+    p1 = deck[:7]
+    p2 = deck[7:14]
+    deck = deck[14:]
+    
+    # each player has 7 cards
+    
     p1 = [deck.pop(0) for _ in range(7)]
     p2 = [deck.pop(0) for _ in range(7)]
 
-    central_card = deck.pop(0)
+    # central card
+
+    central_card = deck.pop(0)    
+    
+    whose_turn = p1
+
+    # we choose how this information is encoded
+    # we decide 0 = player 1, 1 = player 2 
+
     main_loop(p1, p2, deck, central_card, 0)
 
-
-
 def main_loop(p1, p2, deck, central_card, whose_turn):
-    while len(p1)>0 and len(p2)>0:
-        print(f"\nPlayer {whose_turn + 1}'s turn, here is your hand {p1}")
-        print(f"Central card is: {central_card}")
 
+    while p1 and p2:
+        p1_uno_protection = 0
+        p2_uno_protection = 0
 
-        ans = int(input("You have a choice. You can (0) draw or (1) play: "))
-        if ans == 1:
-            valid_play_found = "No :("
-            
-        while valid_play_found == "No :(":
-            player_choice = int(input("Which card to play? (Enter the card number starting from 1): ")) - 1
-            if 0 <= player_choice < len(p1):
-                valid = valid_play(central_card, p1[player_choice])
-                if valid:
-                    central_card = p1.pop(player_choice)
-                    valid_play_found = "Yup"
-                else:
-                    print("Invalid play. Please choose a valid card.")
+        print(f"\nplayer {whose_turn + 1}'s turn, here is your hand: {p1} ")
+        print(f"\ncentral card is: {central_card}")
+
+        # give the user a choice
+        if len(p1) > 1 or len(p2) > 1:
+            ans = int(input("you have a choice. you can (0) draw or (1) play: "))
+            print(f"User input: {ans}")
+
+        elif len(p1) == 1 or len(p2) == 1:
+            ans = int(input("you have a choice. you can (0) draw, (1) play, (3) call uno or (4) call out player: "))
+        print(f"User input: {ans}")
+        
+        if ans == 0:
+            drawn_card = deck.pop(0)
+            p1.append(drawn_card)
+            print(f"you drew a card: {drawn_card}")
+        elif ans == 1:
+            # ask the user for a card to play
+            player_choice = int(input("which card to play? ")) - 1
+
+            if player_choice < 0 or player_choice >= len(p1):
+                print("invalid card choice!")
+                continue
+
+            valid = valid_play(central_card, p1[player_choice])
+            if valid:
+                central_card = p1.pop(player_choice)
+                print(f"you played: {central_card}")
+                plus2_flag = True
+                if central_card[1] == "+2" and plus2_flag==True:
+                    print("The opponent must draw 2 cards!")
+                    p2.append(deck.pop(0))
+                    p2.append(deck.pop(0))
+                    plus2_flag = False
+
             else:
-                print("Invalid card selection. Please try again.")
+                print("you can't put that card!")
 
-        plus2_flag = True
-        if central_card[0] == "+2" and plus2_flag==True:
-            print("The opponent must draw 2 cards!")
-            p2.append(deck.pop(0))
-            p2.append(deck.pop(0))
-            plus2_flag = False
 
-        elif ans == 0:
-            draw_card = deck.pop(0)
-            p1.append(draw_card)
 
+
+        elif ans == 3:
+            if whose_turn == 0:
+                p1_uno_protection += 1
+                print("\n player 1 has called uno!")
+            elif whose_turn == 1:
+                p2_uno_protection += 1
+                print("\n player 2 has called uno!")
+
+        elif ans == 4:
+            if whose_turn == 0 and p2_uno_protection != 1:
+                print("player 1 called out player 2 for forgetting to say uno! he must now pick three cards!")
+                for _ in range(3):
+                    p2_call_out_cards = deck.pop(0)
+                    p2.append(p2_call_out_cards)
+            elif whose_turn == 1 and p1_uno_protection != 1:
+                print("player 2 called out player 1 for forgetting to say uno! he must now pick three cards!")
+                for _ in range(3):
+                    p1_call_out_cards = deck.pop(0)
+                    p1.append(p1_call_out_cards)
+            else:
+                print("the player has said uno! you can't call him out!")
+
+        # switch players
         p1, p2 = p2, p1
         whose_turn = (whose_turn + 1) % 2
 
+        if len(p1) == 0:
+            print("player 1 has won!")
+            break
+        elif len(p2) == 0:
+            print("player 2 has won!")
+            break
 
 
+        # replace player 1 hand with player 2
+
+            # the code that deals with drawing 
 
 def valid_play(card1, card2):
+    # return true if the number or the colour of the cards in the game
     return card1[0] == card2[0] or card1[1] == card2[1]
 
 start_game()
